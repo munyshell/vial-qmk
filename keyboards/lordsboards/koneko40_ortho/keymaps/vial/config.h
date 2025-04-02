bde5c5decf68271455ce0efaffdd6c919708af50
Carlos Munarriz <carlosmunarriz82@gmail.com>
2025-04-02 17:05:19 +0200

    Add lordsboards/koneko40 keyboard

---
+++ keyboards/lordsboards/koneko40_ortho/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x3A, 0xF0, 0x69, 0xEC, 0x7C, 0x8A, 0x54, 0x34}
+
+#define VIAL_UNLOCK_COMBO_ROWS {0, 0}
+#define VIAL_UNLOCK_COMBO_COLS {0, 11}
