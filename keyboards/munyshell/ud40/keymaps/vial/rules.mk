79c90029e1f01123a2fd444d6b3d251f7242ba12
Carlos Munarriz <carlosmunarriz82@gmail.com>
2025-04-15 16:37:54 +0200

    Add ud40 staggered

---
+++ keyboards/munyshell/ud40/keymaps/vial/rules.mk
@@ -0,0 +1,14 @@
+VIA_ENABLE = yes
+VIAL_ENABLE = yes
+LTO_ENABLE = yes
+TAP_DANCE_ENABLE = yes
+COMBO_ENABLE = yes
+QMK_SETTINGS = yes
+KEY_OVERRIDE_ENABLE = no
+CONSOLE_ENABLE = no         # Console for debug
+COMMAND_ENABLE = no         # Commands for debug and configuration
+NKRO_ENABLE = no           # Enable N-Key Rollover
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
+AUDIO_ENABLE = no           # Audio output
+BOOTMAGIC_ENABLE = yes
